a5547e1d5d64b666a3b69caca94b9e4fd16af405
Nick P <npidgeon97@gmail.com>
2026-04-06 11:28:02 -0400

    Performance optimizations, lazy-loading for IG embed, book schema for SEO, reduced motion support for scrolling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://jimmypidgeon.com',
   integrations: [sitemap()],
+  image: {
+    experimentalLayout: 'responsive',
+  },
 });
